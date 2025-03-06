33ecd94da962cc0c4e05dbcd8da7e634fdca3161
Joseph Weiner <jconlanweiner@gmail.com>
2025-03-06 17:45:10 -0500

    Part 1/3
    Not including strace files

---
+++ part1.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main() {
+    const char *filename = "testFile";
+    int file;
+    remove(filename);
+    file = open(filename, O_WRONLY | O_CREAT);
+    close(file);
+    return;
+}
