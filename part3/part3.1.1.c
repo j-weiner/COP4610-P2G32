d1cb9f8f7da40c0a1be0555a5c5e425015a33812
Joseph Weiner <jconlanweiner@gmail.com>
2025-03-20 20:11:17 -0400

    Bar 2D array, adjacency checks

--- part3/part3.1.1.c
+++ part3/part3.1.1.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
 #define Tstools 32
+/*
+#define tables 4;
+#define stools_per_table 8;
+static int bar[tables][stools]={NULL};
+//cell=NULL- Empty seat
+//cell=0 - dirty seat
+//cell>0 - group ID
+//reinitialize cells to NULL after cleaning
+*/
 static bool stool_occupied[Tstools] = {false}; 
+
+
+/*
+static int bar_array[tables][stools];
+bool stools_are_adjacent(int t1, int s1, int t2, int s2) {
+    if( (t1||t2) < 0 || (t1||t2) >= tables || (s1||s2) < 0 || (s1||s2) >= stools) {
+        return false; //input validation
+    } else if(t1==t2 && (s1-1==s2||s1+1==s2)) {
+        return true;//if s1 & s2 are at the same table, & s1 is only one off s2, they are adjacent
+    } else {
+        switch(t1) {
+        case 0:
+            if(t2!=1&&s1!=7&&s2!=0) return false;
+        case 1:
+            if(t2!=)
+            
+        }
+    }
+};
+*/
 
 bool stools_are_adjacent(int s1, int s2) {
     // Return false if out of range, or if same stool
