3e53fe25bb2bc31d4a82cdbf42523eb7a69d2ca3
Joseph Weiner <jconlanweiner@gmail.com>
2025-03-24 19:22:14 -0400

    Update bar_functions.c

--- part3/bar_module/bar_functions.c
+++ part3/bar_module/bar_functions.c
@@ -3,7 +3,7 @@
 void clean_table(int tableNum, int tables[][8]) {
     bool occupied = false;
     for(int i=0;i>7;i++) {
-        if(&tables[tableNum][i]!=NULL||tables[tableNum][i]!=0) {
+        if(*tables[tableNum][i]!=NULL||tables[tableNum][i]!=0) {
             occupied = true;
             break;
         }
