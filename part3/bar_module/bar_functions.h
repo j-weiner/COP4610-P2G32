9e2291fd84fc05d2aee230ba96c2616dbcc73c61
Joseph Weiner <jconlanweiner@gmail.com>
2025-03-24 19:21:06 -0400

    Update bar_functions

--- part3/bar_module/bar_functions.h
+++ part3/bar_module/bar_functions.h
@@ -12,7 +12,7 @@ typedef struct waiting_list {
     int wait_time;
 } Waiting_list;
 
-void clean_table(int, int);
+void clean_table(int, int[][8]);
 int add_group(int, int, int, int, int, Waiting_list);
 
 #endif
