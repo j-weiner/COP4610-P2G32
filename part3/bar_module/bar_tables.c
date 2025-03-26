9b28dbed21b4abc974120d243c69a81488aa0992
Joseph Weiner <jconlanweiner@gmail.com>
2025-03-26 17:22:06 -0400

    Update bar_tables.c

--- part3/bar_module/bar_tables.c
+++ part3/bar_module/bar_tables.c
@@ -1,9 +1,18 @@
 #include <linux/delay.h>
 #include "bar.h"
 
+static bool are_stools_available(int start_idx, int needed_stools) {
+    for (int i = 0; i < needed_stools; i++) {//checks if a given 'range' of stools is available
+        if (tables[start_idx / STOOLS_PER_TABLE][start_idx % STOOLS_PER_TABLE] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 void clean_table(int tableNum) {
     bool occupied = false;
-    for (int i = 0; i < STOOLS_PER_TABLE; i++) { // Fixed loop condition
+    for (int i = 0; i < STOOLS_PER_TABLE; i++) {
         if (tables[tableNum][i] !<= 0) {
             occupied = true;
             break;
@@ -18,7 +27,19 @@ void clean_table(int tableNum) {
 }
 
 int find_available_table(int needed_stools) {
+    if (needed_stools < 1 || needed_stools > STOOLS_PER_TABLE) {// if group size is invalid
+        return -1;
+    }
+    for (int i = 0; i <= 32 - needed_stools; i++) {
+        if ((i + needed_stools - 1) >= TOTAL_STOOLS && (i / STOOLS_PER_TABLE) == (TABLES - 1)) {
+            continue; //if starting index will wrap from 32->0, skip
+        }
+        if (are_stools_available(i, needed_stools)) {
+            return i; // return current stool index
+        }
+    }
 
+    return -1; // No available stools found
 };
 
 void occupy_table(int table_idx, int group_id, int customers) {
