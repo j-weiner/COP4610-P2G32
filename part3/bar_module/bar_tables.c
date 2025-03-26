d89f6017d94f295f738df1d6b5b9efd51ac466aa
Joseph Weiner <jconlanweiner@gmail.com>
2025-03-25 21:27:51 -0400

    Update bar_tables.c

--- part3/bar_module/bar_tables.c
+++ part3/bar_module/bar_tables.c
@@ -4,7 +4,7 @@
 void clean_table(int tableNum) {
     bool occupied = false;
     for (int i = 0; i < STOOLS_PER_TABLE; i++) { // Fixed loop condition
-        if (tables[tableNum][i] != 0) {
+        if (tables[tableNum][i] !<= 0) {
             occupied = true;
             break;
         }
@@ -22,5 +22,8 @@ int find_available_table(int needed_stools) {
 };
 
 void occupy_table(int table_idx, int group_id, int customers) {
-
-};
+    for (int i = 0; i < customers; i++) {
+        int stool_idx = table_idx + i;
+        tables[stool_idx / STOOLS_PER_TABLE][stool_idx % STOOLS_PER_TABLE] = group_id;
+    }
+}
