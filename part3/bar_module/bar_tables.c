d84f03d3a86e802d23645a2c904f06e914f33074
Joseph Weiner <jconlanweiner@gmail.com>
2025-03-26 18:17:43 -0400

    Update bar_tables.c

--- part3/bar_module/bar_tables.c
+++ part3/bar_module/bar_tables.c
@@ -13,7 +13,7 @@ static bool are_stools_available(int start_idx, int needed_stools) {
 void clean_table(int tableNum) {
     bool occupied = false;
     for (int i = 0; i < STOOLS_PER_TABLE; i++) {
-        if (tables[tableNum][i] !<= 0) {
+        if (tables[tableNum][i] > 0) {
             occupied = true;
             break;
         }
